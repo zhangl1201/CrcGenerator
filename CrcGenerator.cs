14eca7be34cbdba544049335e38bdec56b891c4c
Ling <zhangl@whu.edu.cn>
2017-06-14 17:29:48 +0800

    Initial commit

---
+++ CrcGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace CrcGenerator
+{
+    public class CrcGenerator
+    {
+        public static string GetCrcSHA1(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new NullReferenceException("The fileName is null or empty.");
+            }
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException("Can not find the file to compute CRC.");
+            }
+
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            SHA1 sManaged = new SHA1Managed();
+            byte[] crc = sManaged.ComputeHash(fs);
+            fs.Close();
+
+            return BitConverter.ToString(crc).Replace("-", "");
+        }
+
+        public static string GetCrcSHA256(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new NullReferenceException("The fileName is null or empty.");
+            }
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException("Can not find the file to compute CRC.");
+            }
+
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            SHA256 sManaged = new SHA256Managed();
+            byte[] crc = sManaged.ComputeHash(fs);
+            fs.Close();
+
+            return BitConverter.ToString(crc).Replace("-", "");
+        }
+
+        public static string GetCrcSHA512(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new NullReferenceException("The fileName is null or empty.");
+            }
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException("Can not find the file to compute CRC.");
+            }
+
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            SHA512 sManaged = new SHA512Managed();
+            byte[] crc = sManaged.ComputeHash(fs);
+            fs.Close();
+
+            return BitConverter.ToString(crc).Replace("-", "");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CrcGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = string.Empty;
+            bool launchOnCmd = false;
+            if (args.Length >= 1)
+            {
+                filePath = args[0];
+                launchOnCmd = true;
+            }
+            else
+            {
+                Console.WriteLine("Please input the integrated path of the file you want to compute hash code. FilePath:");
+                filePath = Console.ReadLine();
+            }
+            Console.WriteLine(filePath);
+
+            try
+            {
+                Console.WriteLine("SHA1" + "\t" + CrcGenerator.GetCrcSHA1(filePath));
+                Console.WriteLine("SHA256" + "\t" + CrcGenerator.GetCrcSHA256(filePath));
+                Console.WriteLine("SHA512" + "\t" + CrcGenerator.GetCrcSHA512(filePath));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
+            if (!launchOnCmd)
+            {
+                Console.WriteLine("Press any key to exit......");
+                Console.Read();
+            }
+        }
+    }
+}
